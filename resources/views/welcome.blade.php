3a68579245eb263268fea007bdae2b49cdc6b960
RUYUFALBARRAK <99292231+RUYUFALBARRAK@users.noreply.github.com>
2022-11-25 20:13:55 +0300

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
     <img src="img/background_ksu1.png" alt="ksu photo" class="image1">
     <img src="img/background_ksu2.png" alt="ksu photo" class="image1">
     <img src="img/background_ksu3.png" alt="ksu photo" class="image1">
-    <button type="button" class="btn btn-primary btn-lg">Large button</button>
-    
+    <button type="button" class="btn btn-primary btn-lg well">KSU users Login (student and staff)</button>
+    <button type="button" class="btn btn-primary btn-lg "> External users Login (Companies)</button>
     
   </div>
 </main>
