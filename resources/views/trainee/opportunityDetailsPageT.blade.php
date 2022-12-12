d96e91416e55537227c9f61d8dd86d08576a549b
Bushra231 <439204522@student.ksu.edu.sa>
2022-12-12 23:52:17 +0300

    Update opportunityDetailsPageT.blade.php

--- resources/views/trainee/opportunityDetailsPageT.blade.php
+++ resources/views/trainee/opportunityDetailsPageT.blade.php
@@ -6,7 +6,17 @@
     <img src="img/SDAIA.png" alt="Company logo" class="logoCompany">
     <h3 class="spashlist">SAMI (ADVANCED ELECTRONICS)</h3>
     <br><br>
-    
+
+    <div>
+    <label for="validationTooltip01" class="form-label">job title :</label>
+    <label for="validationTooltip01" class="form-label">Software Engineer</label>
+    </div>
+
+    <div>
+    <label for="validationTooltip01" class="form-label">Start Date :</label>
+    <label for="validationTooltip01" class="form-label">Jan, 16, 2022</label>
+    </div>
+
 </div>
  
 
