4217ae6bb8ac360cfa8fa64b0ea4a19e676a8bef
Bushra231 <439204522@student.ksu.edu.sa>
2022-12-12 23:48:13 +0300

    Update opportunityDetailsPageT.blade.php

--- resources/views/trainee/opportunityDetailsPageT.blade.php
+++ resources/views/trainee/opportunityDetailsPageT.blade.php
@@ -3,6 +3,9 @@
 @section('content-training')
 
 <div class='content'>
+    <img src="img/SDAIA.png" alt="Company logo" class="logoCompany">
+    <h3 class="spashlist">SAMI (ADVANCED ELECTRONICS)</h3>
+    <br><br>
     
 </div>
  
