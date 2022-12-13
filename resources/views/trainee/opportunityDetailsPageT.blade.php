e3d02fb5a18b491a42056f78355dc825c7218291
Bushra231 <439204522@student.ksu.edu.sa>
2022-12-13 23:44:26 +0300

    Update opportunityDetailsPageT.blade.php

--- resources/views/trainee/opportunityDetailsPageT.blade.php
+++ resources/views/trainee/opportunityDetailsPageT.blade.php
@@ -5,17 +5,75 @@
 <div class='content'>
     <img src="img/SDAIA.png" alt="Company logo" class="logoCompany">
     <h3 class="spashlist">SAMI (ADVANCED ELECTRONICS)</h3>
+    <br><br><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">job title:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> Software Engineer</label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Start Date:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> Jan, 16, 2022</label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">End Date:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> Aug, 11, 2022</label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">About the company:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">About the role:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Training Requirement:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Incentive:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Work Hours:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Supervisor:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Email:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Phone Number:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Address:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Application deadline:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br><br>
     <br><br>
 
-    <div>
-    <label for="validationTooltip01" class="oppT-form-label">job title :</label>
-    <label for="validationTooltip01" class="oppD-form-label">Software Engineer</label>
-    </div>
-
-    <div>
-    <label for="validationTooltip01" class="oppT-form-label">Start Date :</label>
-    <label for="validationTooltip01" class="oppD-form-label">Jan, 16, 2022</label>
-    </div>
+    <button type="button" class="btn-conf">Confirm</button>
 
 </div>
  
