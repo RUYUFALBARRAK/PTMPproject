367e23b716af0622b602746ad3a0e8f3fa4a2983
Khawlah Alghanim <104341910+khwla12@users.noreply.github.com>
2022-12-15 19:46:52 +0000

    edits

--- resources/views/trainee/ReuqstIdentfaction.blade.php
+++ resources/views/trainee/ReuqstIdentfaction.blade.php
@@ -6,21 +6,22 @@
 <div class="content">
 <table class="table-balqees">
     <tr>
-        <th class="th-balqees">Identification letter</th>
+        <th class="th-khawlah">Identification letter</th> <th class="th-khawlah"></th>
     </tr>
     <tr>
-        <td class="fist-col td-balqees"><button type="button" class="Identification-req-but" > Requst identification letter </button>
+        <td class="fist-col-khawlah td-balqees"><button type="button" class="Identification-req-but" > Requst identification letter </button>
 </td>
-<td class="td-balqees-link"> <a href="#">Vview Identification letter<a> </td>
-        <td class="td-balqees"><a href=<span class="glyphicon glyphicon-download-alt"></span></a></td>
+
+<td class="td-balqees-link"> <a href="#">View Identification letter<a> <a href=<span class="glyphicon glyphicon-download-alt"></span></td>
+  
     </tr>
     <tr>
-        <td class ="fist-col td-balqees">uploaded file:</td>
+        <td class ="fist-col-khawlah td-balqees">uploaded file:</td>
         <td class="td-balqees"><button style="font-size:15px" class="btn btn-outline-secondary" ><i class="glyphicon glyphicon-open"></i> upload file</button></td>
 
     </tr>
     <tr>
-    <td class ="fist-col td-balqees"></td>
+    <td class ="fist-col-khawlah td-balqees"></td>
     <td class=" td-balqees"><button type="button" class="Identification-req-but" > Submit </button>
 </tr>
 
