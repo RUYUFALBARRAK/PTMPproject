9f9ea9fc2de7df5c1cb22912fbb72eef58c2e262
Bushra231 <439204522@student.ksu.edu.sa>
2022-12-17 19:25:46 +0300

    Update opportunityDetailsPageT.blade.php

--- resources/views/trainee/opportunityDetailsPageT.blade.php
+++ resources/views/trainee/opportunityDetailsPageT.blade.php
@@ -4,8 +4,27 @@
 
 <div class='content'>
     <img src="img/SDAIA.png" alt="Company logo" class="logoCompany">
-    <h3 class="spashlist">SAMI (ADVANCED ELECTRONICS)</h3>
+    <h3 class="spashlist">SDAIA (Saudi Data & AL Authority)</h3>
+    <br>
+
+    <span class="rate2" style="margin-left: 84%; margin-top: -3%;">
+        <input type="radio" id="star5" name="rate2" value="5" />
+        <label for="star5" title="text" >5 stars  </label>
+        <input type="radio" id="star4" name="rate2" value="4" />
+        <label for="star4" title="text" >4 stars</label>
+        <input type="radio" id="star3" name="rate2" value="3" />
+        <label for="star3" title="text">3 stars </label>
+        <input type="radio" id="star2" name="rate2" value="2" />
+        <label for="star2" title="text" >2 stars</label>
+        <input type="radio" id="star1" name="rate2" value="1" />
+        <label for="star1" title="text" >1 star</label>
+    </span>
+    <br>
+    <div class="view-reveiws2" style="margin-left: 86.5%; margin-top: 0%;" onClick="">
+            View Reviews
+    </div>
     <br><br><br>
+
 
     <div>
     <label for="validationTooltip01" class="oppT-form-label">job title:</label>
