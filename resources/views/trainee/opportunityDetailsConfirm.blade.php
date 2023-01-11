59ced616dbfa6a78f610bf5e486555ccba013622
Bushra231 <439204522@student.ksu.edu.sa>
2023-01-11 17:28:41 +0300

    Create opportunityDetailsConfirm.blade.php

---
+++ resources/views/trainee/opportunityDetailsConfirm.blade.php
@@ -0,0 +1,95 @@
+@extends('trainee.mainPage')
+
+@section('content-training')
+
+<div class='content'>
+    <img src="img/SDAIA.png" alt="Company logo" class="logoCompany">
+    <h3 class="spashlistB">SDAIA (Saudi Data & AL Authority)</h3>
+    <br>
+
+    <span class="rate2" style="margin-left: 84%; margin-top: -3%;">
+    <span class="fa fa-star fa-lg checked"></span>
+    <span class="fa fa-star fa-lg" style="color:#ccc; text-shadow: 0.5px 0.5px 0 #8f8420;"></span>
+    <span class="fa fa-star fa-lg " style="color:#ccc; text-shadow: 0.5px 0.5px 0 #8f8420;"></span>
+    <span class="fa fa-star fa-lg " style="color:#ccc; text-shadow: 0.5px 0.5px 0 #8f8420;"></span>
+    <span class="fa fa-star fa-lg " style="color:#ccc; text-shadow: 0.5px 0.5px 0 #8f8420;"></span>
+    </span>
+    <br>
+    <div class="view-reveiws2" style="margin-left: 84%; ">
+        <a href="#">View Reviews</a>
+    </div>
+    <br><br><br>
+
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">job title:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> Software Engineer</label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Start Date:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> Jan, 16, 2022</label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">End Date:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> Aug, 11, 2022</label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">About the company:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">About the role:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Training Requirement:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Incentive:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Work Hours:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Supervisor:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Email:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Phone Number:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Address:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br>
+
+    <div>
+    <label for="validationTooltip01" class="oppT-form-label">Application deadline:</label>
+    <label for="validationTooltip01" class="oppD-form-label"> </label>
+    </div><br><br>
+    <br><br>
+
+   
+
+</div>
+
+
+@endsection
