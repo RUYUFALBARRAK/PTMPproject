8236a8a725b8a71056d2cd75d835bc7a07c3a654
Bushra231 <439204522@student.ksu.edu.sa>
2023-01-19 00:08:03 +0300

    Fix the arrow shape

--- resources/views/trainee/opportunityPageTrainee.blade.php
+++ resources/views/trainee/opportunityPageTrainee.blade.php
@@ -79,9 +79,9 @@
 
           <td>
             @if($is_apply)
-              <a href="{{ route('opportunity.confirm' , $opportunitiy->id) }}" class="btn btn-light ml-4"><i class="fa fa-chevron-right"></i></a>
+              <a href="{{ route('opportunity.confirm' , $opportunitiy->id) }}"><span class="fa fa-chevron-right"></span></a>
             @else
-              <a href="{{ route('opportunity.apply' , $opportunitiy->id) }}" class="btn btn-light ml-4"><i class="fa fa-chevron-right"></i></a>
+              <a href="{{ route('opportunity.apply' , $opportunitiy->id) }}"><span class="fa fa-chevron-right"></span></a>
             @endif
           </td>
         
