4e69d60becaab7f7648a5ca86b14e91ac661c886
khwlah12 <alghanimkhawlah12@gmail.com>
2023-02-18 10:06:35 +0000

    tiny edit on its search

--- app/Http/Controllers/khawlahController.php
+++ app/Http/Controllers/khawlahController.php
@@ -44,7 +44,7 @@ class khawlahController extends Controller
         if (Session::has('logincompId')) {
 
             $search_trainee = $_GET['query'];
-            $traineesResult = DB::table('users')->join('requestedopportunity', 'requestedopportunity.trainee_id', '=', 'users.trainee_id')->join('opportunity', 'requestedopportunity.opportunity_id', '=', 'opportunity.id')->Where('opportunity.company_id', '=', session('logincompId'))->Where('statusFormCompany', 'accept')->Where('users.status', 'Ongoing')->where('name', 'LIKE', '%' . $search_trainee . '%')->orWhere('jobTitle', 'LIKE', '%' . $search_trainee . '%')->get();
+            $traineesResult = DB::table('users')->join('requestedopportunity', 'requestedopportunity.trainee_id', '=', 'users.trainee_id')->join('opportunity', 'requestedopportunity.opportunity_id', '=', 'opportunity.id')->Where('opportunity.company_id', '=', session('logincompId'))->Where('statusbycompany', 'accept')->Where('statusbytrainee', 'accept')->Where('users.status', 'Ongoing')->where('name', 'LIKE', '%' . $search_trainee . '%')->orWhere('jobTitle', 'LIKE', '%' . $search_trainee . '%')->get();
             return view('Company/searchlistOfTraineesCompany', compact('traineesResult'));
         }
     }
